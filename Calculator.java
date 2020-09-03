c9d125c18527e6b78a28f24f9679fd107283c9cf
beer <beer_za-2555@hotmail.com>
2020-09-03 10:31:29 +0700

    Create Structure

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator{
+private int op1,op2;
+public int multiply(int num1,int num2){
+return 0;
+}
+public int add(int num1,int num2){ 
+return 0;
+}
+public int subtract(int num1,int num2){ 
+return 0;
+}
+public int divide(int num1,int num2){ 
+return 0;
+}
+}
